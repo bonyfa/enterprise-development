5a5153ea8c481b5959b327c3f2e0fcd49245cf93
bonyfa <125239199+bonyfa@users.noreply.github.com>
2025-10-21 23:42:07 +0400

    Made model for an appointment

--- Hospital/Hospital.Domain/Models/Appointment.cs
+++ Hospital/Hospital.Domain/Models/Appointment.cs
@@ -1,6 +1,32 @@
 namespace Hospital.Models;
 
+//<summary>
+//Model that describe an Appointment
+//</summary>
 public class Appointment
 {
+    //<summary>
+    //Date and time of appointment
+    //</summary>
+    public required DateTime DateAndTime { get; set; }
     
+    //<summary>
+    //Number of office
+    //</summary>
+    public required int NumberOfOffice { get; set; }
+    
+    //<summary>
+    //First or not appointment
+    //</summary>
+    public required bool IsRepeated { get; set; }
+    
+    //<summary>
+    //Patient with an appointment 
+    //</summary>
+    public required Patient Patient { get; set; }
+    
+    //<summary>
+    //Doctor receiving an appointment
+    //</summary>
+    public required Doctor Doctor { get; set; }
 }
